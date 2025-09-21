b7ce679c02bc673957eba96f3123e8f1a73d4915
YTJ-10 <yasirtijani913@gmail.com>
2025-09-21 23:38:04 +0100

    Update 6-multi_languages_loop.js

--- 6-multi_languages_loop.js
+++ 6-multi_languages_loop.js
@@ -1,4 +1,9 @@
-const array = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
-for(i = 0; i < 3; i++){
-    console.log(array[i])
+const lines = [
+  'C is fun',
+  'Python is cool',
+  'JavaScript is amazing'
+];
+
+for (const line of lines) {
+  console.log(line);
 }
